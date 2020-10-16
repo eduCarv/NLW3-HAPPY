9e8eba4f2fd7d69c6eacaacb037c9a3069ccf22d
eduCarv <edu.ascarvalho@gmail.com>
2020-10-16 04:25:33 -0300

    First commit

---
+++ backend/src/routes.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from './config/upload';
+import OrphanagesController from './controllers/OrphanagesControllers';
+
+const routes = Router();
+const upload = multer(uploadConfig);
+
+// Padrões do controller: Index, show, create, update, delete
+
+routes.get('/orphanages', OrphanagesController.index);
+routes.get('/orphanages/:id', OrphanagesController.show);
+routes.post('/orphanages', upload.array('images'), OrphanagesController.create);
+
+export default routes;
